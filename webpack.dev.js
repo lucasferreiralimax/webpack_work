492b4cefcb316eeda38b765fb85792e69be02761
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-18 12:26:08 -0800

    remove MiniCssExtractPlugin and adjustment specific import merge

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: 'development',

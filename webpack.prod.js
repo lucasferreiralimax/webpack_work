924d9796e7737eacd7617dd86c53576fd0cf609c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-07 17:41:04 -0300

    update cssMinimizerPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()],
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   }
 });
